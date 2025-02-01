ab9dc9d90ed682a491ae78d6925fdc6f0b999bcb
usoguacho <warriorgreat2020@gmail.com>
2025-01-31 18:46:10 -0800

    Completed Exercise 1

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,18 @@
 using System;
 
+namespace Bondname
+{
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+
+        Console.Write("What is your last name?");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your names's {lastName}, {firstName} {lastName}."); 
     }
+}
 }
